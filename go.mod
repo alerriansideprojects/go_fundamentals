767d6bf2c99524de9378cdbacb5ec0b8a07d92c4
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-15 11:35:56 -0600

    Add integers and mod file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_fundamentals
+
+go 1.14
---
+++ integers/adder_test.go
@@ -0,0 +1,12 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("Expected '%d' by got '%d'", expected, sum)
+	}
+}
