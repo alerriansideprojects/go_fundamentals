62fdb16a414e531019677d97826a80be9226a50d
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-15 16:29:54 -0600

    Add test for arrays_slices

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,11 +1,13 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestHello(t *testing.T) {
 	assertCorrectMessage := func(t *testing.T, got, want string) {
 		t.Helper()
-	
+
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
@@ -18,7 +20,7 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say 'Hello, World', when an empty string is supplied", func(t *testing.T){
+	t.Run("say 'Hello, World', when an empty string is supplied", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, World"
 
