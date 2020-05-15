34644cb7539e108a9e3ae7605ee699c7ce339b67
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-15 17:58:30 -0600

    Refac tests and add examples for documentation

--- array_slices/sum.go
+++ array_slices/sum.go
@@ -18,3 +18,21 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+// Alternate way to move a variable for return
+//  Expects that return will be a variable called sums
+//  Allows for a little cleaner approach to returns and initializes sums early
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			// numbers[1:] here is calling everything on a slice/array
+			//  until the end of the slice/array
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return
+}
--- array_slices/sum_test.go
+++ array_slices/sum_test.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"testing"
+	"fmt"
 	"reflect"
+	"testing"
 )
 
 func TestSum(t *testing.T) {
@@ -30,6 +31,52 @@ func TestSumAll(t *testing.T) {
 	want := []int{3, 9}
 
 	if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
+			t.Errorf("\nGot: %d\nWant: %v", got, want)
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSum := func(t *testing.T, got, want []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("\nGot: %d\nWant: %v", got, want)
+		}
+	}
+	
+	t.Run("can get sum of slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSum(t, got, want)
+	})
+
+	t.Run("can sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSum(t, got, want)
+	})
+}
+
+func ExampleSum() {
+	numbers := []int{1, 2, 3, 4, 5}
+	sum := Sum(numbers)
+
+	fmt.Println(sum)
+	//Output: 15
+}
+
+func ExampleSumAll() {
+	sumAll := SumAll([]int{1,2}, []int{0,9})
+
+	fmt.Println(sumAll)
+	//Output: [3 9]
+}
+
+func ExampleSumAllTails() {
+	sumAllTails := SumAllTails([]int{1, 2}, []int{0, 9})
+
+	fmt.Println(sumAllTails)
+	//Output: [2 9]
 }
