06b3ab2123271a177a9a84afd3c605c74aa34a09
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-16 11:45:20 -0600

    Add refactor for test and structs/interfaces

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,14 +1,40 @@
 package shapes
 
+import "math"
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
 type Rectangle struct {
 	Width float64
 	Height float64
 }
 
-func rectanglePerimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
-func rectangleArea(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 0
+}
+
+// func Perimeter(rectangle Rectangle) float64 {
+// 	return 2 * (rectangle.Width + rectangle.Height)
+// }
+
+// func Area(rectangle Rectangle) float64 {
+// 	return rectangle.Width * rectangle.Height
+// }
--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -6,34 +6,45 @@ import (
 )
 
 func TestShapes(t *testing.T) {
-	assertCorrectCalculation := func(t *testing.T, got, want float64) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
 		t.Helper()
 
+		got := shape.Area()
+
 		if got != want {
-			t.Errorf("\nGot: %.2f\nWant: %.2f", got, want)
+			t.Errorf("\nGot: %g\nWant: %g", got, want)
+		}
+	}
+
+	checkPerimeter := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+
+		got := shape.Perimeter()
+
+		if got != want {
+			t.Errorf("\nGot: %g\nWant: %g", got, want)
 		}
 	}
 
 	t.Run("calculate perimeter of rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectanglePerimeter(rectangle)
-		want := 40.0
-
-		assertCorrectCalculation(t, got, want)
+		rectangle := Rectangle{10, 10}
+		checkPerimeter(t, rectangle, 40.0)
 	})
 
 	t.Run("calculate area of rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangleArea(rectangle)
-		want := 72.0
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
 
-		assertCorrectCalculation(t, got, want)
+	t.Run("area of circle", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
 
 func ExampleRectanglePerimeter() {
 	rectangle := Rectangle{10.0, 10.0}
-	perimeter := rectanglePerimeter(rectangle)
+	perimeter := rectangle.Perimeter()
 
 	fmt.Println(perimeter)
 	//Output: 40
@@ -41,9 +52,8 @@ func ExampleRectanglePerimeter() {
 
 func ExampleRectangleArea() {
 	rectangle := Rectangle{12.0, 6.0}
-	area := rectangleArea(rectangle)
+	area := rectangle.Area()
 
 	fmt.Println(area)
 	//Output: 72
 }
-
