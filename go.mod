99b4978c78f647fa619ef3d31adf208425971394
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-16 11:51:16 -0600

    Remove unneccessary mod file requires

--- go.mod
+++ go.mod
@@ -2,7 +2,3 @@ module go_fundamentals
 
 go 1.14
 
-require (
-	github.com/gookit/color v1.2.5
-	github.com/gorilla/mux v1.7.4 // indirect
-)
---
+++ structs_methods_interfaces/rectangle.go
@@ -0,0 +1,14 @@
+package shapes
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
