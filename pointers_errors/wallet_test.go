7d53b72043126d2ca4345e197c2198e1eab02032
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-21 17:43:25 -0600

    Add tests and function for a bitcoin wallet

---
+++ pointers_errors/wallet_test.go
@@ -0,0 +1,31 @@
+package wallet
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+	
+		got := wallet.Balance()
+		want := Bitcoin(10)
+	
+		if got != want {
+			t.Errorf("\nGot: %s\nWant: %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("\nGot: %s\nWant: %s", got, want)
+		}
+	})
+}
